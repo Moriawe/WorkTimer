bd454a52bc1d84d206a0abd7e5216951e2985a2d
jennie.lindahl <jennie.lindahl@lynkco.com>
2024-02-05 09:28:39 +0100

    update imports and downgraded needed API level

--- app/src/main/java/com/moriawe/worktimer2/presentation/component/TimeCard.kt
+++ app/src/main/java/com/moriawe/worktimer2/presentation/component/TimeCard.kt
@@ -14,8 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.moriawe.worktimer2.domain.model.TimeCardItem
+
 
 @Composable
 fun TimeCard(time: TimeCardItem, onClick: () -> Unit) {
